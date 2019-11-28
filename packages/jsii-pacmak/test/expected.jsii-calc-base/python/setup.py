90596fb05539039001863effec30fd73422d3246
Mitchell Valine <valinm@amazon.com>
2019-11-27 16:02:51 -0800

    Handle Python Dependency Versions Correctly
    Change logic of python dependency version ranges. Parse semver number
    either as specific version or range and output to setup.py.
    
    Fix #676

--- packages/jsii-pacmak/test/expected.jsii-calc-base/python/setup.py
+++ packages/jsii-pacmak/test/expected.jsii-calc-base/python/setup.py
@@ -32,7 +32,7 @@ kwargs = json.loads("""
     "install_requires": [
         "jsii~=0.20.10",
         "publication>=0.0.3",
-        "scope.jsii-calc-base-of-base~=0.20.10"
+        "scope.jsii-calc-base-of-base==0.20.10"
     ],
     "classifiers": [
         "Intended Audience :: Developers",
