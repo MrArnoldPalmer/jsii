7f484cafe257e2146e69fe50ffffe7b94a5fb34f
Mitchell Valine <mitchellvaline@yahoo.com>
2020-08-05 21:32:31 -0700

    AST Definition

--- packages/jsii-pacmak/lib/targets/golang/package.ts
+++ packages/jsii-pacmak/lib/targets/golang/package.ts
@@ -1,19 +1,17 @@
-import { CodeMaker } from 'codemaker';
 import { Assembly } from 'jsii-reflect';
-import { join } from 'path';
 import { EmitContext } from './emit-context';
 import { ReadmeFile } from './readme-file';
-import { Module, Submodule } from './module';
+import { RootModule } from './module';
 
 export class Package {
   public readonly packageName: string;
-  public readonly rootModule: Module;
+  public readonly rootModule: RootModule;
 
   public constructor(public readonly assembly: Assembly) {
     this.packageName = this.assembly.name
       .replace('@', '')
       .replace(/[^a-z0-9_.]/gi, '');
-    this.rootModule = new Module(assembly);
+    this.rootModule = new RootModule(assembly);
   }
 
   public emit({ code }: EmitContext): void {
@@ -21,32 +19,6 @@ export class Package {
       new ReadmeFile(this.packageName, this.assembly.readme.markdown);
     }
 
-    const packageFile = `${join(...this.packageName.split('.'))}.go`;
-    code.openFile(packageFile);
-    code.line(`package ${this.packageName}`);
-    code.line();
     this.rootModule.emit(code);
-    code.closeFile(packageFile);
-
-    this.emitSubmodules(code, this.rootModule);
-  }
-
-  public emitSubmodules(
-    code: CodeMaker,
-    parent: Module,
-    parentPath = '',
-  ): void {
-    parent.submodules.forEach((submodule: Submodule) => {
-      const nextPath = `${parentPath}/${submodule.name}`;
-      const filename = `${nextPath}.go`;
-
-      code.openFile(filename);
-      code.line();
-      submodule.emit(code);
-      code.closeFile(filename);
-
-      // recurse into next level
-      this.emitSubmodules(code, submodule, nextPath);
-    });
   }
 }
--- packages/jsii-pacmak/lib/targets/golang/types/class.ts
+++ packages/jsii-pacmak/lib/targets/golang/types/class.ts
@@ -1,47 +1,142 @@
 import { Method, Property, ClassType } from 'jsii-reflect';
 import { CodeMaker } from 'codemaker';
-import { TypeMapper } from './interface';
-import { GoType } from './go-type';
+import { GoTypeRef } from './go-type-reference';
+import { GoType, GoEmitter } from './go-type';
+import { Module } from '../module';
 
-export class GoClass extends GoType {
-  public constructor(public type: ClassType) {
-    super(type);
+export class ClassField {
+  public constructor(public readonly parent: GoClass) {}
+}
+
+export class ClassProperty extends ClassField {
+  public readonly references?: GoTypeRef;
+  public constructor(parent: GoClass, public readonly property: Property) {
+    super(parent);
+
+    if (property.type) {
+      this.references = new GoTypeRef(parent.parent.root, property.type);
+    }
   }
 
-  public emit(code: CodeMaker): void {
-    code.openBlock(`type ${this.localName} struct`);
+  public emit(code: CodeMaker) {
+    const name = this.property.name;
+    const type =
+      this.references?.scopedName(this.parent.parent) ??
+      this.property.toString();
 
-    Object.values(this.type.getProperties()).forEach((property) =>
-      this.emitClassProperty(code, property),
-    );
+    code.line(`${name} ${type}`);
+  }
+}
 
-    code.closeBlock();
-    code.line();
+export class ClassMethod extends ClassField {
+  public readonly references?: GoTypeRef;
+  public constructor(parent: GoClass, public readonly method: Method) {
+    super(parent);
 
-    Object.values(this.type.getMethods()).forEach((method) =>
-      this.emitClassMethod(code, method),
-    );
+    if (method.returns.type) {
+      this.references = new GoTypeRef(parent.parent.root, method.returns.type);
+    }
   }
 
-  private emitClassProperty(code: CodeMaker, property: Property) {
-    const type = new TypeMapper(property.type).mapType();
+  public emit(code: CodeMaker) {
+    const name = this.method.name;
+    const type =
+      this.references?.scopedName(this.parent.parent) ?? this.method.toString();
 
-    code.line(`${property.name} ${type}`);
-  }
-
-  private emitClassMethod(code: CodeMaker, method: Method) {
-    const returns = method.returns.type.void
-      ? ''
-      : ` ${new TypeMapper(method.returns.type).mapType()}`;
-    const instanceArg = this.localName.substring(0, 1);
+    const instanceArg = this.parent.name.substring(0, 1);
 
     // TODO: Method Arguments
     // NOTE: May need to capitalize method name
     code.openBlock(
-      `func (${instanceArg} *${this.localName}) ${method.name}()${returns}`,
+      `func (${instanceArg} *${this.parent.name}) ${name}()${type}`,
     );
     code.line(`// jsiiruntime.methodcall(${instanceArg})`);
     code.closeBlock();
     code.line();
   }
+}
+
+/*
+ * Class wraps a Typescript class as a Go custom struct type  TODO rename?
+ */
+export class GoClass extends GoType implements GoEmitter {
+  public readonly properties: ClassProperty[];
+  public readonly methods: ClassMethod[];
+
+  public constructor(parent: Module, public type: ClassType) {
+    super(parent, type);
+
+    this.properties = Object.values(this.type.getProperties()).map(
+      (prop) => new ClassProperty(this, prop),
+    );
+
+    this.methods = Object.values(this.type.getMethods()).map(
+      (method) => new ClassMethod(this, method),
+    );
+  }
+
+  public emit(code: CodeMaker): void {
+    code.openBlock(`type ${this.name} struct`);
+
+    this.properties.forEach((property) => property.emit(code));
+
+    code.closeBlock();
+    code.line();
+
+    this.methods.forEach((method) => method.emit(code));
+
+    code.line();
+  }
+
+  public get dependencies(): Module[] {
+    return [
+      ...this.properties.reduce((accum: Module[], property) => {
+        return property.references?.type?.parent
+          ? [...accum, property.references?.type.parent]
+          : accum;
+      }, []),
+      ...this.methods.reduce((accum: Module[], method) => {
+        return method.references?.type?.parent
+          ? [...accum, method.references?.type.parent]
+          : accum;
+      }, []),
+    ];
+  }
+
+  // public emit({ code }: GoEmitContext): void {
+  //   code.openBlock(`type ${this.localName()} struct`);
+
+  //   Object.values(this.type.getProperties()).forEach((property) =>
+  //     this.emitClassProperty(code, property),
+  //   );
+
+  //   code.closeBlock();
+  //   code.line();
+
+  //   Object.values(this.type.getMethods()).forEach((method) =>
+  //     this.emitClassMethod(code, method),
+  //   );
+  // }
+
+  // private emitClassProperty(code: CodeMaker, property: Property) {
+  //   const type = new TypeMapper(property.type).emit();
+
+  //   code.line(`${property.name} ${type}`);
+  // }
+
+  // private emitClassMethod(code: CodeMaker, method: Method) {
+  //   const returns = method.returns.type.void
+  //     ? ''
+  //     : ` ${new TypeMapper(method.returns.type).emit()}`;
+  //   const instanceArg = this.localName().substring(0, 1);
+
+  //   // TODO: Method Arguments
+  //   // NOTE: May need to capitalize method name
+  //   code.openBlock(
+  //     `func (${instanceArg} *${this.localName()}) ${method.name}()${returns}`,
+  //   );
+  //   code.line(`// jsiiruntime.methodcall(${instanceArg})`);
+  //   code.closeBlock();
+  //   code.line();
+  // }
 }
